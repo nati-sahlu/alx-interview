086261567acc58fa4130db3bc909a73e04257fdb
nati-sahlu <natansahlu5@gmail.com>
2025-05-01 14:02:44 -0700

    task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,14 +4,14 @@
 
 def minOperations(n):
     """
-    Function minOperations
-    Returns an integer
+    Calculates the fewest number of operations needed to result
+    in exactly n H characters using only Copy All and Paste operations.
     """
     result = 0
     x = 2
     while n > 1:
         while n % x == 0:
             result += x
-            n /= x
+            n //= x
         x += 1
     return result
