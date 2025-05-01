ac241c518bb820b24481d9dead19a2642b1ed838
nati-sahlu <natansahlu5@gmail.com>
2025-05-01 13:56:46 -0700

    task

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Minimum Operations
+    """
+
+def minOperations(n):
+    """
+    Function minOperations
+    Returns an integer
+    """
+    result = 0
+    x = 2
+    while n > 1:
+        while n % x == 0:
+            result += x
+            n /= x
+        x += 1
+    return result
