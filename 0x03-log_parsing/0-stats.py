22575b154a44c3d5a6f3873f69d7a40722725703
nati-sahlu <natansahlu5@gmail.com>
2025-05-09 11:56:56 -0700

    task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,57 +1,50 @@
 #!/usr/bin/python3
 """
-Reads stdin line by line and computes metrics.
-
-Input format:
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+Log parsing script that reads stdin line by line
+and prints log stats every 10 lines and on keyboard interrupt.
 """
+
 import sys
+import re
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-total_size = 0
-line_count = 0
+def output(log: dict) -> None:
+    """
+    Helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
-def print_stats():
-    """Prints the metrics."""
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+if __name__ == "__main__":
+    regex = re.compile(
+        r'\d{1,3}(?:\.\d{1,3}){3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+\] '
+        r'"GET /projects/260 HTTP/1.1" (\d{3}) (\d+)'
+    )
 
-try:
-    for line in sys.stdin:
-        parts = line.strip().split()
-        if len(parts) < 7:
-            continue
+    line_count = 0
+    log = {
+        "file_size": 0,
+        "code_frequency": {str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
+    }
 
-        status = parts[-2]
-        size = parts[-1]
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if match:
+                line_count += 1
+                code, size = match.groups()
+                log["file_size"] += int(size)
 
-        try:
-            total_size += int(size)
-        except ValueError:
-            continue
+                if code in log["code_frequency"]:
+                    log["code_frequency"][code] += 1
 
-        if status in status_codes:
-            status_codes[status] += 1
-
-        line_count += 1
-        if line_count % 10 == 0:
-            print_stats()
-
-except KeyboardInterrupt:
-    print_stats()
-    raise
-
-finally:
-    print_stats()
+                if line_count % 10 == 0:
+                    output(log)
+    except KeyboardInterrupt:
+        output(log)
+        raise
+    finally:
+        output(log)
 
