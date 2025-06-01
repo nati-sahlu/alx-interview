16c40392e1f3e78a70a288ac80d2c43816c5a367
nati-sahlu <natansahlu5@gmail.com>
2025-06-01 12:32:31 -0700

    task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,30 +2,26 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-request(url, (err, res, body) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  const characters = JSON.parse(body).characters;
-
-  const printCharacter = (index) => {
-    if (index >= characters.length) return;
-
-    request(characters[index], (err, res, body) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
       console.log(JSON.parse(body).name);
-      printCharacter(index + 1);
-    });
-  };
+      req(arr, i + 1);
+    }
+  });
+};
 
-  printCharacter(0);
-});
-
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
+  }
+);
